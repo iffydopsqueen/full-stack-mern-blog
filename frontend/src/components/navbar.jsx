a1b7e76ace26f7f63b36373bcbd4c5376ca2486e
Iffy Eleweke <omaleweks@gmail.com>
2024-04-12 11:38:43 -0400

    Added the '<Link>' component for our routes.

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router-dom";
+import logo from "../imgs/logo.png";
+
 const Navbar = () => {
     return (
         <nav className="navbar">
-            
+
+            <Link to="/" className="flex-none w-10">
+                <img src={logo} className="w-full" />
+            </Link>
         </nav>
     )
 }
