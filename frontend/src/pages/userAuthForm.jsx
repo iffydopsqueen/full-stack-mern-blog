5f136bda161d2a86090fc62c686a24231b97d095
Iffy Eleweke <omaleweks@gmail.com>
2024-04-17 14:33:52 -0400

    Added the relevant buttons and links to the sign-in and sign-up pages.

--- frontend/src/pages/userAuthForm.jsx
+++ frontend/src/pages/userAuthForm.jsx
@@ -1,4 +1,6 @@
 import InputBox from "../components/input";
+import googleIcon from "../imgs/google.png";
+import { Link } from "react-router-dom";
 
 const UserAuthForm = ({ type }) => {
     return (
@@ -32,6 +34,41 @@ const UserAuthForm = ({ type }) => {
                     placeholder="Password"
                     icon="fi-sr-lock"
                 />
+
+                <button
+                    className="btn-dark center mt-14"
+                    type="submit"
+                >
+                    { type.replace("-", " ")}
+                </button>
+
+                <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                    <hr className="w-1/2 border-black" />
+                    <p>or</p>
+                    <hr className="w-1/2 border-black" />
+                </div>
+
+                <button className="btn-dark flex items-center justify-center gap-4 w-[90%] center">
+                    <img src={googleIcon} className="w-5" />
+                    continue with google
+                </button>
+
+                {
+                    type == "sign-in" ? 
+                    <p className="mt-6 text-dark-grey text-xl text-center">
+                        Don't have an account ?
+                        <Link to="/signup" className="underline text-black text-xl ml-1">
+                            Join now
+                        </Link>
+                    </p>
+                    : 
+                    <p className="mt-6 text-dark-grey text-xl text-center">
+                        Already a member ?
+                        <Link to="/signin" className="underline text-black text-xl ml-1">
+                            Sign in
+                        </Link>
+                    </p>
+                }
             </form>
         </section>
     )
